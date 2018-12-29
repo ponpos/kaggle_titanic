06f12aa936d2b1bcfefa3bf77c56b8dad073f818
yoshimi sakai <sakai002202@gmail.com>
2018-12-29 23:35:10 +0900

    SibSp Parchをドロップ

--- source/simple_dt.py
+++ source/simple_dt.py
@@ -28,11 +28,14 @@ test["FamilySize"]=test["SibSp"]+test["Parch"]+1
 train.drop("Name",axis=1,inplace=True)
 train.drop("Cabin",axis=1,inplace=True)
 train.drop("Ticket",axis=1,inplace=True)
+train.drop("SibSp",axis=1,inplace=True)
+train.drop("Parch",axis=1,inplace=True)
 
 test.drop("Name",axis=1,inplace=True)
 test.drop("Cabin",axis=1,inplace=True)
 test.drop("Ticket",axis=1,inplace=True)
-
+test.drop("SibSp",axis=1,inplace=True)
+test.drop("Parch",axis=1,inplace=True)
 
 train_data = train.values
 x_train = train_data[:, 2:]
